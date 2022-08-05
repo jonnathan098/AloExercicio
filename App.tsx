ad85f4d96ce221ee637bc582df035babf3c322d3
jonnathan <jhonthancarlos@gmail.com>
2022-08-05 11:27:42 -0300

    finalisado

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 
 import React from 'react';
-import TouchableHighlightExample from './src/telas/Exemplos';
-import App from './src/telas/exerciso';
 import Navegacao from './src/telas/Navegaçao';
-import TelaAvaliaçao from './src/telas/TelaAvaliaçao';
 
 const app: React.FC = () => {
   return(
-    <TelaAvaliaçao/>
+    <Navegacao/>
   );
 };
 export default app;
